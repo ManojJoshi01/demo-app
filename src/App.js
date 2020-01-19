14c3b6e6cf880b5e0939ca5cabf8bf4fae6189ff
Manoj Joshi <60060694+ManojJoshi01@users.noreply.github.com>
2020-01-19 15:09:45 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hi</h1>
+      <h2>I am Manoj Joshi</h2>
+      <button onClick={() => console.log("Login Successfull")}>
+        Go to Login
+      </button>
+      <select>
+        <option>Pune</option>
+        <option>Mumbai</option>
+        <option>Banglore</option>
+        <option>Pune</option>
+        <option>Pune</option>
+      </select>
+    </div>
+  );
+}
